74a01f50e4b6fea6e847c84ef302df8deac86d89
David Hiller <HillerDavid@users.noreply.github.com>
2019-01-17 17:34:37 -0500

    Updates

---
+++ precache-manifest.2dda56989f6e146e8beec20698987223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae3733258257ccef2eee2b20c77ac79a",
+    "url": "/ClickyGame/static/media/1942.ae373325.ttf"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "b6a91e75f4337767c2b5",
+    "url": "/ClickyGame/static/js/main.b6a91e75.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "b6a91e75f4337767c2b5",
+    "url": "/ClickyGame/static/css/main.a3fda5bf.chunk.css"
+  },
+  {
+    "revision": "e805686dac13793cc466d1620c17892b",
+    "url": "/ClickyGame/index.html"
+  }
+];
